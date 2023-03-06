525755ac4a08650a928c0738d87823adef57eb69
`Vlad` <86572964+Hikfound@users.noreply.github.com>
2023-03-06 09:14:06 +0200

    версия3

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,11 +7,22 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Random random = new Random();
-            for (int i=1;i<=10;i++)
+            //for (int i=1;i<=10;i++)
+            //{
+            //    int x = random.Next(-40, 40);
+            //    Console.WriteLine(x);
+            //}
+            int count = 0;
+            for (int i=1;i<=15;i++)
             {
-                int x = random.Next(-40, 40);
+                double x = -20+50*random.NextDouble();
                 Console.WriteLine(x);
+                if (x>0)
+                    {
+                    count++;
+                }
             }
+            Console.WriteLine{$"Количество положит.={count}"};
             Console.Read();
         }
     }
